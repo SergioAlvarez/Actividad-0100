f0fec80fd5e078db1ef1941ab2796d08017643ae
SergioA <sergioalvarez316@gmail.com>
2014-04-07 09:41:57 +0200

    Implementada la funcionalidad de que el usuario introduzca el númro de bolas, el radio y la posición de las bolas son aleatorias.

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.util.Random;
+import java.util.ArrayList;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -23,31 +25,45 @@ public class BallDemo
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numBalls)
     {
         int ground = 400;   // position of the ground line
-
+        ArrayList<BouncingBall> arrayBolas = new ArrayList<>();
+        Random aleatorio = new Random();
+        int numeroAleatorio = aleatorio.nextInt(101);
         myCanvas.setVisible(true);
-
+       
+        
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
         // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
-
-        // make them bounce
-        boolean finished =  false;
-        while(!finished) {
-            myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
-            }
+        for(int i=0; i < numBalls; i++)
+        {
+            BouncingBall ball = new BouncingBall(aleatorio.nextInt(100),aleatorio.nextInt(150),aleatorio.nextInt(200),Color.RED, ground, myCanvas);
+            arrayBolas.add(ball);
+           arrayBolas.get(i).draw();
         }
+       
+        boolean finished =  false;
+         while(!finished) {
+           myCanvas.wait(50);           
+        for(int i=0; i<arrayBolas.size();i++)
+        {
+            arrayBolas.get(i).move();
+           if(arrayBolas.get(i).getXPosition() >= 550)
+           {
+               finished = true;
+            }
+            } 
+        }
+
     }
+    
 }
+   
+        
+       
+        
+
+
